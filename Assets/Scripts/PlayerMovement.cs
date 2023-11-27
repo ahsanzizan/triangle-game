c6e0dcddf226053bb30e3d624e6a3fcbe8bbba9f
Ahsan <ahsanaz461@gmail.com>
2023-11-27 10:01:26 +0700

    feat: Add player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb;
+    private Vector2 movement, moveDir;
+
+    [Header("Attributes")]
+    public float moveSpeed = 10f;
+    public float lookOffset;
+
+    private void Start()
     {
-        
+        if (rb == null)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal"), moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY).normalized;
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
+    }
+
+    private void LookAtMouse()
+    {
+        // Get player to mouse position direction
+        moveDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotation = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotation + lookOffset);
+    }
+
+    private void Update()
+    {
+        LookAtMouse();
     }
 }
